3e31a6fd64d6072990290e15d1840a611c12e4f2
Alex Pushkin <ifc@suz-ppk.ru>
2024-10-16 13:20:55 +1000

    Добавьте файлы проекта.

---
+++ Console1125delegate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console1125delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Делегаты и лямбда-выражения в C#
+            
+        }
+    }
+}
